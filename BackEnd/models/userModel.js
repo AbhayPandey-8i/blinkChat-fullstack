f06aa234a390d656781155f247d492f727f35411
Abhay Pandey <pandeyabhay055@gmail.com>
2026-03-30 19:48:52 +0530

    add user models

---
+++ BackEnd/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+
+const userModel = new mongoose.Schema(
+    
+    {
+      fullName:{
+        type:String,
+        required:true
+      },
+      username:{
+        type:String,
+        required:true,
+        unique:true
+      },
+      Password:{
+        type:String,
+        required:true,
+      },
+      profilePhoto:{
+        type:String,
+        default:""
+      },
+      gender:{
+        type:String,
+        enum:["male" , "female"],
+        required:true
+      },
+    },
+    
+{timestamps:true})
+
+export const User = mongoose.model("User", userModel)
