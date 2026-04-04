0c062126c2bdf3315607bb0b47875e7c0d209fad
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-05 00:18:37 +0530

    fix routing

--- BackEnd/routes/messageRoute.js
+++ BackEnd/routes/messageRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { sendMessage } from "../controllers/messageControllers.js"
+import isAuthenticated from "../middlewares/isAuthenticated.js"
 
 const router = express.Router()
 
-router.route("/send/:id").post(sendMessage)
+router.route("/send/:id").post(isAuthenticated, sendMessage)
 
 export default router
