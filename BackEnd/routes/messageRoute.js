54d73e4ad0a5f3896e518e64f0108dc232734484
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-05 23:46:26 +0530

    update routes

--- BackEnd/routes/messageRoute.js
+++ BackEnd/routes/messageRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { sendMessage } from "../controllers/messageControllers.js"
+import { getMessage, sendMessage } from "../controllers/messageControllers.js"
 import isAuthenticated from "../middlewares/isAuthenticated.js"
 
 const router = express.Router()
 
 router.route("/send/:id").post(isAuthenticated, sendMessage)
+router.route("/:id").get(isAuthenticated, getMessage)
 
 export default router
