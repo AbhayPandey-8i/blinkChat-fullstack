6c30e6ec2f23c2857368b55325db097429374d59
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-06 21:25:28 +0530

    add login ui

---
+++ FrontEnd/src/components/Login.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Login = () => {
+    return (
+        <div className="min-w-96 mx-auto">
+            <div className="w-full p-6 rounded-lg shadow-md 
+    bg-white/10 backdrop-blur-lg border border-white/20">
+
+                <h1 className="text-3xl font-bold text-center">
+                    Login
+                </h1>
+
+                <form action="">
+
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text' >Username</span>
+                        </label>
+                        <input className='w-full input input-bordered h-10' type="text" placeholder='Enter Your Username' />
+                    </div>
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text' >Password</span>
+                        </label>
+                        <input className='w-full input input-bordered h-10' type="password" placeholder='Enter Your Password' />
+                    </div>
+
+                    <div className='flex items-center my-4' >
+                        <div className='flex items-center' >
+                            <p>Male</p>
+                            <input type="checkbox" defaultChecked className="checkbox mx-2" />
+                        </div>
+
+                        <div className='flex items-center'>
+                            <p>Female</p>
+                            <input type="checkbox" defaultChecked className="checkbox mx-2" />
+                        </div>
+                    </div>
+
+                    <div className=' text-center'>
+
+                        <p>New to BlinkChat ? <Link to="/register" >Signup
+                            <button className='btn btn-block btn-sm mt-2 text-lg border-slate-700'>Signup</button>
+
+                        </Link></p>
+                    </div>
+
+                </form>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Login
