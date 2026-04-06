5a490aca5828de0f5d7a7acf7fe8f2326f3077d6
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-07 00:41:45 +0530

    more dependencies

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -1,6 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
+import axios from 'axios'
+
+
+
 const Login = () => {
+
+const navigate = useNavigate()
+const [user, setUser] = useState({
+    username:"",
+    password:"",
+})
+
+ const onSubmitHandler = async (e) => {
+   e.preventDefault()
+
+  try {
+            const res = await axios.post(`http://localhost:8080/api/v1/user/login`, user, {
+                headers: {
+                    "Content-Type": 'application/json'
+                },
+                withCredentials: true
+            
+            });
+            navigate("/")
+       console.log(res)
+           
+        } catch (error) {
+            toast.error(error.response.data.message)
+            console.log(error)
+        }
+
+   setUser({
+    username:"",
+    password:"",
+   })
+ }
+
     return (
         <div className="min-w-96 mx-auto">
             <div className="w-full p-6 rounded-lg shadow-md 
@@ -10,39 +47,29 @@ const Login = () => {
                     Login
                 </h1>
 
-                <form action="">
+                <form onSubmit={onSubmitHandler} action="">
 
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text' >Username</span>
                         </label>
-                        <input className='w-full input input-bordered h-10' type="text" placeholder='Enter Your Username' />
+                        <input value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})} className='w-full input input-bordered h-10' type="text" placeholder='Enter Your Username' />
                     </div>
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text' >Password</span>
                         </label>
-                        <input className='w-full input input-bordered h-10' type="password" placeholder='Enter Your Password' />
+                        <input value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})} className='w-full input input-bordered h-10' type="password" placeholder='Enter Your Password' />
                     </div>
 
-                    <div className='flex items-center my-4' >
-                        <div className='flex items-center' >
-                            <p>Male</p>
-                            <input type="checkbox" defaultChecked className="checkbox mx-2" />
-                        </div>
+                    
 
-                        <div className='flex items-center'>
-                            <p>Female</p>
-                            <input type="checkbox" defaultChecked className="checkbox mx-2" />
-                        </div>
-                    </div>
+                    <div className=' text-center my-4'>
 
-                    <div className=' text-center'>
-
-                        <p>New to BlinkChat ? <Link to="/register" >Signup
-                            <button className='btn btn-block btn-sm mt-2 text-lg border-slate-700'>Signup</button>
+                        <p>New to BlinkChat ? <Link to="/register" > <span className='text-blue-400'>Signup</span>
 
                         </Link></p>
+                            <button type='submit' className='btn btn-block btn-sm mt-2 text-lg border-slate-700'>Login</button>
                     </div>
 
                 </form>
--- FrontEnd/src/components/Signup.jsx
+++ FrontEnd/src/components/Signup.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { use, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios"
+import toast from "react-hot-toast"
 
 const Signup = () => {
+    const navigate = useNavigate()
+    const [user, setUser] = useState({
+        fullName: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+        gender: "",
+    })
+
+    const handleCheckBox = (gender) => {
+        setUser({ ...user, gender })
+    }
+
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault()
+
+        try {
+            const res = await axios.post(`http://localhost:8080/api/v1/user/register`, user, {
+                headers: {
+                    "Content-Type": 'application/json'
+                },
+                withCredentials: true
+            
+            });
+           if (res.data.success) {
+            navigate("/login")
+            toast.success("Account created successfully")
+
+           }
+        } catch (error) {
+            toast.error(error.response.data.message)
+            console.log(error)
+        }
+
+        setUser({
+            fullName: "",
+            username: "",
+            password: "",
+            confirmPassword: "",
+            gender: "",
+        })
+    }
+
+
     return (
         <div className="min-w-96 mx-auto">
             <div className="w-full p-6 rounded-lg shadow-md 
@@ -11,48 +58,54 @@ const Signup = () => {
                     Signup
                 </h1>
 
-                <form action="">
+                <form onSubmit={onSubmitHandler} action="">
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text' >Fullname</span>
                         </label>
-                        <input className='w-full input input-bordered h-10' type="text" placeholder='Enter Your Name' />
+                        <input value={user.fullName} onChange={(e) => setUser({ ...user, fullName: e.target.value })} className='w-full input input-bordered h-10' type="text" placeholder='Enter Your Name' />
                     </div>
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text' >Username</span>
                         </label>
-                        <input className='w-full input input-bordered h-10' type="text" placeholder='Enter Your Username' />
+                        <input value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} className='w-full input input-bordered h-10' type="text" placeholder='Enter Your Username' />
                     </div>
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text' >Password</span>
                         </label>
-                        <input className='w-full input input-bordered h-10' type="password" placeholder='Enter Your Password' />
+                        <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} className='w-full input input-bordered h-10' type="password" placeholder='Enter Your Password' />
                     </div>
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text' >Confirm Password</span>
                         </label>
-                        <input className='w-full input input-bordered h-10' type="password" placeholder='Confirm Your Password' />
+                        <input value={user.confirmPassword} onChange={(e) => setUser({ ...user, confirmPassword: e.target.value })} className='w-full input input-bordered h-10' type="password" placeholder='Confirm Your Password' />
                     </div>
                     <div className='flex items-center my-4' >
                         <div className='flex items-center' >
                             <p>Male</p>
-                            <input type="checkbox" defaultChecked className="checkbox mx-2" />
+                            <input
+                                checked={user.gender === "male"}
+                                onChange={() => handleCheckBox("male")}
+                                type="radio" className="checkbox mx-2" />
                         </div>
 
                         <div className='flex items-center'>
                             <p>Female</p>
-                            <input type="checkbox" defaultChecked className="checkbox mx-2" />
+                            <input
+                                checked={user.gender === "female"}
+                                onChange={() => handleCheckBox("female")}
+                                type="radio" className="checkbox mx-2" />
                         </div>
                     </div>
 
                     <div className=' text-center'>
 
-                        <p>Already have an account ? <Link to="/login" >Login
-                            <button className='btn btn-block btn-sm mt-2 text-lg border-slate-700'>Login</button>
+                        <p>Already have an account ? <Link to="/login"> <span className='text-blue-400'>Login</span>
                         </Link>
+                            <button type='submit' className='btn btn-block btn-sm mt-2 text-lg border-slate-700'>Signup</button>
                         </p>
                     </div>
 
