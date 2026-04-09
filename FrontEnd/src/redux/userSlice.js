c1b869ac6f17a8dc52d19ea673ad1c7053b63dcd
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-10 00:31:59 +0530

    fix know issues

--- FrontEnd/src/redux/userSlice.js
+++ FrontEnd/src/redux/userSlice.js
@@ -3,22 +3,30 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
     name:"user",
     initialState:{
-        authUser:null,
+        authUser: JSON.parse(localStorage.getItem("user")) || null,
         otherUsers:null,
+        selectedUser:null,
     },
-
-
 
     reducers:{
         setAuthUser:(state,action)=>{
             state.authUser = action.payload
+
+            if (action.payload) {
+                localStorage.setItem("user", JSON.stringify(action.payload))
+            } else {
+                localStorage.removeItem("user")
+            }
         },
 
         setOtherUsers:(state,action)=>{
             state.otherUsers = action.payload
+        },
+        setSelectedUser:(state, action)=>{
+            state.selectedUser = action.payload
         }
     }
 })
 
-export const {setAuthUser, setOtherUsers} = userSlice.actions;
-export default userSlice.reducer
+export const {setAuthUser, setOtherUsers, setSelectedUser} = userSlice.actions;
+export default userSlice.reducer;
