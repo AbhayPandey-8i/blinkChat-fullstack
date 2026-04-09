5c18e0bad5cd96ba88b30ca723835602eda63d42
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-10 00:30:59 +0530

    fix bug

--- BackEnd/controllers/messageControllers.js
+++ BackEnd/controllers/messageControllers.js
@@ -32,7 +32,7 @@ export const sendMessage = async (req, res) => {
          //socket.io
 
         return res.status(201).json({
-            message:"Message sent successfully"
+            newMessage
         })
            
     } catch (error) {
