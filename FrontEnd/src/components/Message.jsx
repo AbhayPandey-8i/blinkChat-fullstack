9cc5c20c0726fa45b977fd5f8c5fb42295d67136
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-10 00:31:21 +0530

    new features

--- FrontEnd/src/components/Message.jsx
+++ FrontEnd/src/components/Message.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
 
-const Message = () => {
+const Message = ({message}) => {
+  const scroll = useRef()
+  const {authUser, selectedUser} = useSelector(store=>store.user)
+  
+  useEffect(() => {
+    scroll.current?.scrollIntoView({behavior:"smooth"})
+  }, [message])
+  
+
   return (
    
-<div className="chat chat-end">
+<div ref={scroll} className={`chat ${authUser?._id === message?.senderId ? "chat-end" : "chat-start"}`}>
   <div className="chat-image avatar">
     <div className="w-10 rounded-full">
       <img
         alt="Tailwind CSS chat bubble component"
-        src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+        src={message?.senderId ===  authUser?._id ? authUser?.profilePhoto : selectedUser?.profilePhoto}
       />
     </div>
   </div>
@@ -16,7 +25,7 @@ const Message = () => {
      
     <time className="text-xs opacity-50">12:45</time>
   </div>
-  <div className="chat-bubble">You were the Chosen One!</div>
+  <div className="chat-bubble"> {message?.message} </div>
  
 </div>
 
--- FrontEnd/src/components/MessageContainer.jsx
+++ FrontEnd/src/components/MessageContainer.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Sendinput from './Sendinput'
 import Messages from './Messages'
+import { useDispatch, useSelector } from 'react-redux'
+import store from '../redux/store'
+import { setSelectedUser } from '../redux/userSlice'
 
 const MessageContainer = () => {
+  const {selectedUser} = useSelector(store=>store.user)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    return() => dispatch(setSelectedUser(null))
+  }, [])
+  
   return (
-    
-       <div className='md:min-w-[550px] flex flex-col'>
 
-       <div className='flex gap-2 items-center  bg-black/40  px-4 py-2 mb-2 ' >
+    <>
+    {
+      selectedUser !== null ? (<div className='md:min-w-[550px] flex flex-col'>
+
+       <div className='flex gap-2 items-center  bg-black/40  px-4 py-2 mb-2 h-16 overflow-hidden ' >
          <div className='avatar'>
            <div className='w-10 rounded-full avatar-online'>
-            <img src="https://i.pinimg.com/736x/54/86/d9/5486d987b94707df3e352c2de0a11914.jpg" alt="" />
+            <img src={selectedUser?.profilePhoto} alt="" />
            </div>
          </div>
         <div className='avatar '>
-          <div className='flex gap-2 flex-1 items-center'>
-            <p>Anushka</p>
+          <div className=' flex gap-2 flex-1 items-center'>
+          <p className='truncate' > {selectedUser?.fullName} </p>
           </div>
         </div>
        </div>
        <Messages/>
        <Sendinput/>
-    </div>
+    </div>) : (
+      <h1>Lets start convo baby</h1>
+    )
+    }
+    </>
+    
+       
     
   )
 }
