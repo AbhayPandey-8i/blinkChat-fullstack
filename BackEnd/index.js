1a6aedfc0c6becba6fb2d96f4067c61f7fbe7508
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-12 18:53:15 +0530

    fix known issue

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,16 +2,30 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/database.js"
 import userRoute from "./routes/userRoute.js"
-import cookieParser from "cookie-parser"
 import messageRoute from "./routes/messageRoute.js"
+import cookieParser from "cookie-parser"
 import cors from "cors"
+import http from "http"
+import { Server } from "socket.io"
 
-dotenv.config({})
+dotenv.config()
+
 const app = express()
 
+// ✅ DEFINE PORT HERE
 const PORT = process.env.PORT || 5000
 
-//middlewares
+const server = http.createServer(app)
+
+// socket setup
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true
+  }
+})
+
+// middleware
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
@@ -19,12 +33,17 @@ app.use(cors({
   credentials: true
 }))
 
-//routes
+// routes
 app.use("/api/v1/user", userRoute)
-app.use("/api/v1/message", messageRoute )
+app.use("/api/v1/message", messageRoute)
 
-app.listen(PORT, () => {
-    connectDB()
-  console.log(`Server is running at port ${PORT}`)
-}
-) 
+// socket connection
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id)
+})
+
+// ✅ USE server.listen
+server.listen(PORT, () => {
+  connectDB()
+  console.log(`Server running on port ${PORT}`)
+})
