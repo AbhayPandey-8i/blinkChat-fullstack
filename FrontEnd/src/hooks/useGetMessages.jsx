7e48904e72c0ffb63ef15c60535902065609c963
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-13 19:29:56 +0530

    real time message

--- FrontEnd/src/hooks/useGetMessages.jsx
+++ FrontEnd/src/hooks/useGetMessages.jsx
@@ -8,19 +8,25 @@ import { setMessages } from '../redux/messageSlice'
  const {selectedUser} = useSelector(store=>store.user)
  const dispatch = useDispatch()
 
-   useEffect(() => {  
-    const fetchMessages = async () => {
-      try {
-        axios.defaults.withCredentials=true
-        const res = await axios.get(`http://localhost:8080/api/v1/message/${selectedUser?._id}`)
-        console.log(res)
-        dispatch(setMessages(res.data))
-      } catch (error) {
-        console.log(error)
-      }
+useEffect(() => {  
+  if (!selectedUser?._id) return; // 🔥 IMPORTANT
+
+  const fetchMessages = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+
+      const res = await axios.get(
+        `http://localhost:8080/api/v1/message/${selectedUser._id}`
+      );
+
+      dispatch(setMessages(res.data || []));
+    } catch (error) {
+      console.log(error);
     }
-    fetchMessages()
-   }, [selectedUser])
+  };
+
+  fetchMessages();
+}, [selectedUser, dispatch]);
    
  }
  
---
+++ FrontEnd/src/hooks/useGetRealTimeMessage.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { setMessages } from "../redux/messageSlice"
+
+const useGetRealTimeMessage = () => {
+    const {socket} = useSelector(store=>store.socket)
+    const {messages} = useSelector(store=>store.message)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+  if (!socket) return;
+
+  const handleNewMessage = (newMessage) => {
+    dispatch(setMessages(prev => [...prev, newMessage]));
+  };
+
+  socket.on("newMessage", handleNewMessage);
+
+  return () => {
+    socket.off("newMessage", handleNewMessage);
+  };
+}, [socket, dispatch]);
+  
+}
+
+export default useGetRealTimeMessage
