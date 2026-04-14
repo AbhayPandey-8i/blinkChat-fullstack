a0e9a6ddbdd9ca620873f3e2b26f745e60046f5a
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-14 17:30:31 +0530

    Project Finished

--- FrontEnd/src/redux/store.js
+++ FrontEnd/src/redux/store.js
@@ -1,18 +1,51 @@
-import {configureStore} from "@reduxjs/toolkit"
-import useReducer from "./userSlice.js"
+import {combineReducers, configureStore} from "@reduxjs/toolkit"
+import userReducer from "./userSlice.js"
 import messageReducer from "./messageSlice.js"
 import socketReducer from "./socketSlice"
 
-const store = configureStore({
-    reducer:{
-        user:useReducer,
-        message:messageReducer,
-        socket:socketReducer
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: false
-        })
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import storageModule from 'redux-persist/lib/storage'
+
+const storage = storageModule.default ?? storageModule
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage, // ✅ use same variable
+  whitelist: ['user'],
+}
+
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    message: messageReducer,
+    socket: socketReducer
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+console.log("storage object:", storage);
+console.log("storage.getItem:", storage?.getItem);
+export const persistor = persistStore(store); 
 
 export default store
